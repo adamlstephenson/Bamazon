d0b937a9c79521a9c5b7d4a86e632198fdf5befb
Adam Stephenson <astephensonx@gmail.com>
2018-05-02 18:13:47 -0500

    both inquirer messages added

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -24,10 +24,26 @@ connection.connect(function (err) {
 
 function displayItems() {
     var itemQuery = "SELECT item_id, product_name, price FROM products";
-    
-    connection.query(itemQuery, function(err, res) {
+
+    connection.query(itemQuery, function (err, res) {
         for (var i = 0; i < res.length; i++) {
             console.log("Item ID: " + res[i].item_id + " || Product: " + res[i].product_name + " || Price: " + res[i].price)
         }
+        cashierFlow();
     });
+}
+
+function cashierFlow() {
+    inquirer.prompt([
+        {
+            name: "idnumber",
+            type: "input",
+            message: "Please enter the item ID of your desired purchase"
+        },
+        {
+            name: "quantity",
+            type: "input",
+            message: "How many units would you like to buy?"
+        }
+    ])
 }
