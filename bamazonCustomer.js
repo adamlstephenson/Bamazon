1f28897663135d84477b50e48165e5feb0139a24
Adam Stephenson <astephensonx@gmail.com>
2018-05-01 20:53:57 -0500

    database connected

---
+++ bamazonCustomer.js
@@ -0,0 +1,17 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+// Database Connection
+//============================================================================================
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "",
+    databse: "bamazon"
+});
+
+connection.connect(function (err) {
+    if (err) throw err;
+});
