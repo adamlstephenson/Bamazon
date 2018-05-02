4b52030e5fcfc791318551d767d059663da060cc
Adam Stephenson <astephensonx@gmail.com>
2018-05-01 23:14:07 -0500

    items displaying to console

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -9,9 +9,25 @@ var connection = mysql.createConnection({
     port: 3306,
     user: "root",
     password: "",
-    databse: "bamazon"
+    database: "bamazon"
 });
 
 connection.connect(function (err) {
     if (err) throw err;
+    displayItems();
 });
+
+//============================================================================================
+
+// Functions
+// ===========================================================================================
+
+function displayItems() {
+    var itemQuery = "SELECT item_id, product_name, price FROM products";
+    
+    connection.query(itemQuery, function(err, res) {
+        for (var i = 0; i < res.length; i++) {
+            console.log("Item ID: " + res[i].item_id + " || Product: " + res[i].product_name + " || Price: " + res[i].price)
+        }
+    });
+}
