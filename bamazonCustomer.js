bbf86675848b4a8aebdcdfff67f652b99632ed0f
Adam Stephenson <astephensonx@gmail.com>
2018-05-04 22:44:42 -0500

    selectItem function working

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -29,21 +29,55 @@ function displayItems() {
         for (var i = 0; i < res.length; i++) {
             console.log("Item ID: " + res[i].item_id + " || Product: " + res[i].product_name + " || Price: " + res[i].price)
         }
-        cashierFlow();
+        console.log("....................................................")
+        selectItem();
     });
 }
 
-function cashierFlow() {
+function selectItem() {
     inquirer.prompt([
         {
             name: "idnumber",
             type: "input",
             message: "Please enter the item ID of your desired purchase"
         },
+    ]).
+        then(function (inquirerResponse) {
+            var itemId = inquirerResponse.idnumber;
+
+            var query = "SELECT * FROM products WHERE ?";
+            connection.query(query, { item_id: itemId }, function (err, res) {
+                if (err) throw err;
+
+                console.log("Product Name: " + res[0].product_name)
+                console.log("Price: " + res[0].price)
+                console.log("....................................................")
+
+                if (res.length === 0) {
+                    console.log("Please select a valid Item ID")
+                }
+                checkStock();
+            })
+        })
+}
+
+function checkStock() {
+
+    inquirer.prompt([
         {
             name: "quantity",
             type: "input",
             message: "How many units would you like to buy?"
         }
-    ])
+    ]).
+        then(function (inquirerResponse) {
+            var numOfUnits = inquirerResponse.quantity;
+
+            var query = "SELECT * FROM products WHERE ?";
+            connection.query(query, { item_id: itemId }, function (err, res) {
+                if (err) throw err;
+
+                
+            })
+        })
 }
